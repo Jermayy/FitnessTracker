7686a9fb7a3b93d92a6f7f34818287d23f5994d7
Jeremy <jeremyVDLRC@gmail.com>
2020-09-13 22:02:16 +1000

    change local host address

--- server.js
+++ server.js
@@ -14,11 +14,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessTracker", { useNewUrlParser: true });
 
 require('./routes/api-routes')(app)
 require('./routes/html-routes')(app)
-
 
 
 
