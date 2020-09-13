6af12c30585762b2a12a4dd922955b519674ab00
Jeremy <jeremyVDLRC@gmail.com>
2020-09-13 17:54:13 +1000

    added html routes to server.js

--- server.js
+++ server.js
@@ -20,8 +20,14 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { 
 
 // Navigating Application
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+  });
 app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/exercise.html"));
+  });
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
   });
 
 
@@ -80,11 +86,6 @@ db.Workout.find({})
     res.json(err);
 })
 })
-
-
-// getting last workout
-app.get("//")
-
 
 
 
