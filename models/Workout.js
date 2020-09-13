3171022ad56f696bf9d4e0f7f6954388f991eb93
Jeremy <jeremyVDLRC@gmail.com>
2020-09-13 18:37:54 +1000

    commit

--- models/Workout.js
+++ models/Workout.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    date: Date,
+    day: {
+        type: Date,
+    default: Date.now()},
     exercises:[
         {
             type: Schema.Types.ObjectId,
             ref:"Exercise"
         }
-    ]
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0
+    }
 
 
 })
